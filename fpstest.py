72db2630901634a90edca17ca0c3af8cbeab8387
xFrah <fdimonaco309@hotmail.com>
2022-11-14 18:40:11 +0100

    fps_counter vanilla

--- fpstest.py
+++ fpstest.py
@@ -4,26 +4,26 @@ import datetime
 from imutils.video import WebcamVideoStream
 import cv2 as cv
 
-subprocess.call(['v4l2-ctl -d /dev/video1 --set-fmt-video=width=640,height=480,pixelformat=MJPG'], shell=True)
-time.sleep(1)
-print("Set video format successfully?")
-
-cam_props = {'gain': 0, 'exposure_auto': 1, 'exposure_absolute': 25}
-for key in cam_props:
-    subprocess.call(['v4l2-ctl -d /dev/video1 -c {}={}'.format(key, str(cam_props[key]))],
-                    shell=True)
-    time.sleep(1)
-    print(f"Set {key} to {cam_props[key]}")
+##subprocess.call(['v4l2-ctl -d /dev/video1 --set-fmt-video=width=640,height=480,pixelformat=MJPG'], shell=True)
+##time.sleep(1)
+##print("Set video format successfully?")
+##
+##cam_props = {'gain': 0, 'exposure_auto': 1, 'exposure_absolute': 25}
+##for key in cam_props:
+##    subprocess.call(['v4l2-ctl -d /dev/video1 -c {}={}'.format(key, str(cam_props[key]))],
+##                    shell=True)
+##    time.sleep(1)
+##    print(f"Set {key} to {cam_props[key]}")
 
 capture = WebcamVideoStream(src=1).start()
 
 # width, height = rescale_frame(640, 480, 50)
 # print(capture.set(cv.CAP_PROP_FRAME_WIDTH, 640))
 # print(capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480))
-print(capture.stream.set(cv.CAP_PROP_FPS, 120))
-# print(capture.set(cv.CAP_PROP_BUFFERSIZE, 1))
-print(capture.stream.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M', 'J', 'P', 'G')))
-print(capture.stream.get(cv.CAP_PROP_FPS))
+##print(capture.stream.set(cv.CAP_PROP_FPS, 120))
+### print(capture.set(cv.CAP_PROP_BUFFERSIZE, 1))
+##print(capture.stream.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('M', 'J', 'P', 'G')))
+##print(capture.stream.get(cv.CAP_PROP_FPS))
 # #print(capture.set(cv.CAP_PROP_AUTO_EXPOSURE, 0.25))
 # print(capture.set(cv.CAP_PROP_EXPOSURE, -11))
 # print(capture.set(cv.CAP_PROP_GAIN, 100))
