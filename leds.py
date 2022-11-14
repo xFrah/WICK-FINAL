432fd909702c9fa2a0fec71029105c141a5532ab
xFrah <fdimonaco309@hotmail.com>
2022-11-14 11:39:06 +0100

    working leds

--- leds.py
+++ leds.py
@@ -11,21 +11,17 @@ from lib import neopixel_spidev as np
 from lib.pixelbuf import wheel
 
 # Init 56 LEDs on SPI bus 2, cs 0 with colors ordered green, red, blue
-with np.NeoPixelSpiDev(0, 0, n=24, pixel_order=np.GRB) as pixels:
+with np.NeoPixelSpiDev(0, 0, n=48, pixel_order=np.GRB) as pixels:
     try:
         while True:
             color = input("Insert color: ")
             if color == "r":
                 pixels.fill((255, 0, 0))
-                pixels.fill((255, 0, 0))
             elif color == "g":
-                pixels.fill((0, 255, 0))
                 pixels.fill((0, 255, 0))
             elif color == "b":
                 pixels.fill((0, 0, 255))
-                pixels.fill((0, 0, 255))
             elif color == "w":
-                pixels.fill((255, 255, 255))
                 pixels.fill((255, 255, 255))
     except KeyboardInterrupt:
         pass
