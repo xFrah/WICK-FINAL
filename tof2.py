f23f92f65a203045e404f56ac0d635c844620b09
xFrah <fdimonaco309@hotmail.com>
2022-11-15 18:49:26 +0100

    tof2

--- tof2.py
+++ tof2.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from vl53l5cx.vl53l5cx import VL53L5CX
@@ -15,17 +16,26 @@ print(f"Initialised ({time.time() - t:.1f}s)")
 
 # Ranging:
 driver.start_ranging()
-print(driver.nb_target_per_zone)
+driver.set_ranging_frequency_hz(60)
+driver.set_integration_time_ms(5)
+print(f"Ranging frequency: {driver.get_ranging_frequency_hz()} Hz")
+print(f"Integration time: {driver.get_integration_time_ms()} ms")
 
+count = 0
+start = datetime.datetime.now()
 while True:
     if driver.check_data_ready():
         ranging_data = driver.get_ranging_data()
 
-        for i in range(16):
-            print(f"Zone : {i: >3d}, "
-                  f"Status : {ranging_data.target_status[driver.nb_target_per_zone * i]: >3d}, "
-                  f"Distance : {ranging_data.distance_mm[driver.nb_target_per_zone * i]: >4.0f} mm")
+        count += 1
+        if count == 100:
+            print("FPS: ", 100 / (datetime.datetime.now() - start).total_seconds())
+            start = datetime.datetime.now()
+            count = 0
 
-        print("")
+        # for i in range(16):
+        #     print(f"Zone : {i: >3d}, "
+        #           f"Status : {ranging_data.target_status[driver.nb_target_per_zone * i]: >3d}, "
+        #           f"Distance : {ranging_data.distance_mm[driver.nb_target_per_zone * i]: >4.0f} mm")
 
     time.sleep(0.005)
