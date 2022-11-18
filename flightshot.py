62417f116acd1cefd42a6f16852fbdb762510753
xFrah <fdimonaco309@hotmail.com>
2022-11-18 13:35:42 +0100

    4x4

--- flightshot.py
+++ flightshot.py
@@ -149,11 +149,9 @@ def setup_led():
 
 # change leds gradually to green
 def change_to_green(pixels):
-    print(print(pixels))
-    pixels.fill((0, 0, 0))
-    pixels.show()
     for i in range(0, 255, 5):
         pixels.fill((0, i, 0))
+        pixels.show()
         time.sleep(0.03)
 
 
@@ -161,15 +159,15 @@ def change_to_green(pixels):
 def black_from_green(pixels):
     for i in range(0, 255, 5)[::-1]:
         pixels.fill((0, i, 0))
+        pixels.show()
         time.sleep(0.03)
 
 
 # change leds gradually to green
 def change_to_red(pixels):
-    pixels.fill((0, 0, 0))
-    pixels.show()
     for i in range(0, 255, 5):
         pixels.fill((i, 0, 0))
+        pixels.show()
         time.sleep(0.03)
 
 
@@ -177,6 +175,7 @@ def change_to_red(pixels):
 def black_from_red(pixels):
     for i in range(0, 255, 5)[::-1]:
         pixels.fill((i, 0, 0))
+        pixels.show()
         time.sleep(0.03)
 
 
