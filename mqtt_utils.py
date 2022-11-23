cbeb37f6b54d507bdafa57aa3859dd5b982c9df0
xFrah <fdimonaco309@hotmail.com>
2022-11-23 16:43:36 +0100

    Done!

--- mqtt_utils.py
+++ mqtt_utils.py
@@ -23,6 +23,10 @@ def on_connect(client, userdata, flags, rc):
         print("Bad connection Returned code= ", rc)
 
 
+def on_connect_fail(client, userdata, flags, rc):
+    print("Connection failed")
+
+
 def setup_mqtt(ip, client_id="mqtt_user", password="Gaspardo1801", port=1883, timeout=10, connection_timeout=100000):
     client: mqtt.Client = None
     start = datetime.datetime.now()
@@ -31,6 +35,7 @@ def setup_mqtt(ip, client_id="mqtt_user", password="Gaspardo1801", port=1883, ti
         try:
             client = mqtt.Client(client_id=client_id)
             client.on_connect = on_connect
+            client.on_connect_fail = on_connect_fail
             client.username_pw_set(client_id, password)
         except:
             print("\n[ERROR] Error while configuring MQTT client, retrying in 5 seconds...")
