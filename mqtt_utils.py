d562aa72d0c8820c3d0984a42f58371b5661ac03
xFrah <fdimonaco309@hotmail.com>
2022-11-23 12:29:25 +0100

    Done!

--- mqtt_utils.py
+++ mqtt_utils.py
@@ -24,7 +24,7 @@ def setup_mqtt(ip, client_id="mqtt_user", password="Beam2020", port=1883, timeou
         try:
             client = mqtt.Client(client_id=client_id, clean_session=True, userdata=None, protocol=mqtt.MQTTv311,
                                  transport="tcp")
-            client.on_connect = on_connect
+            #client.on_connect = on_connect
             # todo client.username_pw_set(client_id, password)
         except:
             print("\n[ERROR] Error while configuring MQTT client, retrying in 5 seconds...")
