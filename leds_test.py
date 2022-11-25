0420f4d55161056f102c869a06b519d9a454baf0
xFrah <fdimonaco309@hotmail.com>
2022-11-25 11:04:27 -0400

    Complete rewrite in OOP

--- leds_test.py
+++ leds_test.py
@@ -1,5 +1,6 @@
 from new_led_utils import LEDs
+from rpi_ws281x import Color
 
 leds = LEDs(start_yellow_loading=False)
-leds.fill((255, 255, 255))
+leds.colorWipe(Color(255, 255, 255), wait_ms=50)
 
