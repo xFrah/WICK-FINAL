689cd51388c7f219dce821cb58047596d3ddb13e
xFrah <fdimonaco309@hotmail.com>
2022-12-07 18:02:17 +0100

    Done!

--- test_servo.py
+++ test_servo.py
@@ -1,5 +1,6 @@
 import time
 
+from adafruit_motor import servo
 from board import SCL, SDA
 import busio
 
@@ -12,12 +13,14 @@ i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus)
 
 # Set the PWM frequency to 60hz.
-pca.frequency = 60
+pca.frequency = 50
+
+servo0 = servo.Servo(pca.channels[0])
 
 # Set the PWM duty cycle for channel zero to 50%. duty_cycle is 16 bits to match other PWM objects
 # but the PCA9685 will only actually give 12 bits of resolution.
 #pca.channels[1].duty_cycle = 0x7FFF
 
 while True:
-    pca.channels[0].angle = input("Angle: ")
+    servo0.angle = input("Angle: ")
     time.sleep(0.05)
