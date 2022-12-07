ec44cfa114bddb55a3cb5303b9ed1edf2841e3b8
xFrah <fdimonaco309@hotmail.com>
2022-12-07 17:48:31 +0100

    Done!

--- test_servo.py
+++ test_servo.py
@@ -12,7 +12,7 @@ i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus)
 
 # Set the PWM frequency to 60hz.
-pca.frequency = 50
+pca.frequency = 60
 
 # Set the PWM duty cycle for channel zero to 50%. duty_cycle is 16 bits to match other PWM objects
 # but the PCA9685 will only actually give 12 bits of resolution.
