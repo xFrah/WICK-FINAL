e046f078790f0d8abd92f4a61f80964e06540489
xFrah <fdimonaco309@hotmail.com>
2022-12-15 18:54:06 +0100

    Done!

--- pro.py
+++ pro.py
@@ -4,12 +4,11 @@ import time
 
 import cv2 as cv
 
-import helpers
+#import helpers
 import tof_utils
-from data_utils import config_and_data
-from edgetpu_utils import inference
-from flightshot import setup
-from tof_utils import get_trash_level
+#from data_utils import config_and_data
+#from edgetpu_utils import inference
+#from tof_utils import get_trash_level
 from watchdog import ping
 
 
@@ -44,9 +43,20 @@ def tof_buffer_update(new_matrix, tof_buffer, average_matrix):
                     average_matrix[i][j] += new_matrix[i][j] / len(tof_buffer)
     return average_matrix
 
+def setup():
+    """
+    It sets up the camera, the LED strip, the VL53L0X sensor, the MQTT client, the TensorFlow interpreter, and the data manager
+    :return: leds, interpreter, camera, vl53, initial_background, empty_tof_buffer, datamanager
+    """
+    vl53 = tof_utils.tof_setup()
+    print("[INFO] Setup complete!")
+    #background = camera.grab_background(return_to_black=False)
+    print("[INFO] Background grabbed!")
+    return vl53
+
 
 def main():
-    leds, interpreter, camera, vl53, background, _, dm = setup()
+    vl53 = setup()
     thread = threading.current_thread()
     thread.setName("Main")
     print(f'[INFO] Main thread "{thread}" started.')
@@ -72,50 +82,50 @@ def main():
                     last_movement = datetime.datetime.now()
                     movement = False
                     print("[INFO] Movement stopped")
-                    buffer = camera.grab_background()
-                    if buffer is not None and len(buffer) > 0:
-                        frame = buffer[-1]
-                        rect, diff = helpers.get_diff(frame, background)
-                        if (rect is not None) and (diff is not None):
-                            x, y, w, h = rect
-                            imgcopy = frame.copy()
-                            cropped = imgcopy[y:y + h, x:x + w]
-                            cv.rectangle(imgcopy, (x, y), (x + w - 1, y + h - 1), 255, 2)
-
-                            if not (cropped.shape[0] > 0 and cropped.shape[1] > 0):
-                                continue
-                            try:
-                                cropped = cv.cvtColor(cropped, cv.COLOR_BGR2RGB)
-                            except:
-                                print("[ERROR] Cropped image is not a valid image")
-                                buffer.clear()
-                                print("[INFO] Waiting for movement...")
-                                continue
-
-                            # label, score = inference(cropped, interpreter)
-                            # print(f"[INFO] Class: {label}, score: {int(score * 100)}%")
-
-                            show_results(imgcopy, diff, cropped=cropped)
-
-                            # todo open servos and make thing fall
-
-                            leds.change_to_white()
-                            background = camera.grab_background(return_to_black=False)
-                            leds.black_from_white()
-                            # if label == config_and_data["current_class"]:
-                            #     leds.change_to_green()
-                            # else:
-                            #     leds.change_to_red()
-                            #     config_and_data["wrong_class_counter"] += 1
-                            # background = camera.grab_background(return_to_black=False)
-                            # if label == config_and_data["current_class"]:
-                            #     leds.black_from_green()
-                            # else:
-                            #     leds.black_from_red()
-                        else:
-                            print("[INFO] Object not found.")
-                            show_results(frame, diff)
-                            background = camera.grab_background(return_to_black=True)
+                    # buffer = camera.grab_background()
+                    # if buffer is not None and len(buffer) > 0:
+                    #     frame = buffer[-1]
+                    #     rect, diff = helpers.get_diff(frame, background)
+                    #     if (rect is not None) and (diff is not None):
+                    #         x, y, w, h = rect
+                    #         imgcopy = frame.copy()
+                    #         cropped = imgcopy[y:y + h, x:x + w]
+                    #         cv.rectangle(imgcopy, (x, y), (x + w - 1, y + h - 1), 255, 2)
+                    #
+                    #         if not (cropped.shape[0] > 0 and cropped.shape[1] > 0):
+                    #             continue
+                    #         try:
+                    #             cropped = cv.cvtColor(cropped, cv.COLOR_BGR2RGB)
+                    #         except:
+                    #             print("[ERROR] Cropped image is not a valid image")
+                    #             buffer.clear()
+                    #             print("[INFO] Waiting for movement...")
+                    #             continue
+                    #
+                    #         # label, score = inference(cropped, interpreter)
+                    #         # print(f"[INFO] Class: {label}, score: {int(score * 100)}%")
+                    #
+                    #         show_results(imgcopy, diff, cropped=cropped)
+                    #
+                    #         # todo open servos and make thing fall
+                    #
+                    #         leds.change_to_white()
+                    #         background = camera.grab_background(return_to_black=False)
+                    #         leds.black_from_white()
+                    #         # if label == config_and_data["current_class"]:
+                    #         #     leds.change_to_green()
+                    #         # else:
+                    #         #     leds.change_to_red()
+                    #         #     config_and_data["wrong_class_counter"] += 1
+                    #         # background = camera.grab_background(return_to_black=False)
+                    #         # if label == config_and_data["current_class"]:
+                    #         #     leds.black_from_green()
+                    #         # else:
+                    #         #     leds.black_from_red()
+                    #     else:
+                    #         print("[INFO] Object not found.")
+                    #         show_results(frame, diff)
+                    #         background = camera.grab_background(return_to_black=True)
 
                     # avg, percentage = get_trash_level(vl53)
                     # print(f"[INFO] {avg}mm, {percentage}%")
@@ -126,7 +136,7 @@ def main():
                     #               "timestamp": str(now.isoformat()),
                     #               "images": (ddd if len(ddd) < 20 else ddd[:20])  # + ([imgcopy] if imgcopy is not None else []),
                     #               })
-                    buffer.clear()
+                    # buffer.clear()
                     print("[INFO] Waiting for movement...")
             ping(thread)
 
