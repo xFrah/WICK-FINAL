b98a1fa7e8ebf8f681c682aa9fc481ed2b6e308c
xFrah <fdimonaco309@hotmail.com>
2022-12-23 17:59:09 +0100

    Added PRO script

--- pro.py
+++ pro.py
@@ -77,13 +77,14 @@ def get_diff_2(i1, i2):
         area = cv.contourArea(c)
         if area > 40:
             print("Countour area: " + str(area))
-            #x, y, w, h = cv.boundingRect(c)
-            #cv.rectangle(image1, (x, y), (x + w, y + h), (36, 255, 12), 2)
-            #cv.rectangle(image2, (x, y), (x + w, y + h), (36, 255, 12), 2)
-            #cv.drawContours(mask, [c], 0, (0, 255, 0), 3)
-            #cv.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
+            # x, y, w, h = cv.boundingRect(c)
+            # cv.rectangle(image1, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            # cv.rectangle(image2, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            # cv.drawContours(mask, [c], 0, (0, 255, 0), 3)
+            # cv.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
     # apply mask thresh to filled_after
-    filled_after = cv.bitwise_and(thresh, i1)
+    filled_after = cv.bitwise_and(filled_after, filled_after, mask=thresh)
+    #filled_after = cv.bitwise_and(thresh, i1)
     return filled_after
 
 
