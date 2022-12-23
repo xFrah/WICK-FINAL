6c2a6de08237592853358b8f4cd256c67f202475
xFrah <fdimonaco309@hotmail.com>
2022-12-23 17:19:43 +0100

    Added PRO script

--- pro.py
+++ pro.py
@@ -77,7 +77,7 @@ def get_diff_2(image1, image2):
         area = cv.contourArea(c)
         if area > 40:
             x, y, w, h = cv.boundingRect(c)
-            #cv.rectangle(image1, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv.rectangle(image1, (x, y), (x + w, y + h), (36, 255, 12), 2)
             #cv.rectangle(image2, (x, y), (x + w, y + h), (36, 255, 12), 2)
             cv.drawContours(mask, [c], 0, (0, 255, 0), -1)
             cv.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
